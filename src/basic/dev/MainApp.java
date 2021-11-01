2b2ebef430f9861004344ca5c0565e8e59d1f6a0
DUNG <dungnguyen9903@gmail.com>
2021-11-01 15:03:06 +0700

    first commit

---
+++ src/basic/dev/MainApp.java
@@ -0,0 +1,29 @@
+package basic.dev;
+
+public class MainApp {
+
+	public static void main(String[] args) {
+//		Thông tin một sinh viên
+		String tenSinhVien;
+		String maSinhVien;
+		int cmnd;
+		String tenLop;
+		String tenTruong;
+		float diemTB;
+		
+//      Thông tin công nhân
+		String tenCongNhan;
+		String diaChi;
+		int soDienThoai;
+		float heSoLuong;
+		
+//      Thông tin một chiếc xe
+		String tenXe;
+		String hangXe;
+		int giaXe;
+		String mauSon;
+		
+
+	}
+
+}
